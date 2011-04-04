078b34994d92fc8bb6108dec089bc8586b7ee774
super <super@lynhurtig.dk>
2011-04-04 14:03:00 +0000

    Fixed Thomas' Change

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,8 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		print "hello martin";
-		$this->load->view('welcome_message');
+            $this->load->view('welcome_message');
 	}
 }
 
