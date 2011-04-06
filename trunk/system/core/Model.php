194416644c70feacbc34a4cc42a0c6e918d37a3e
bigslott@gmail.com <bigslott@gmail.com@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-06 14:51:18 +0000

    autocomplete til netbeans tilføjet

--- trunk/system/core/Model.php
+++ trunk/system/core/Model.php
@@ -24,6 +24,21 @@
  * @author		ExpressionEngine Dev Team
  * @link		http://codeigniter.com/user_guide/libraries/config.html
  */
+/**
+
+* @property CI_Loader $load
+
+* @property CI_Form_validation $form_validation
+
+* @property CI_Input $input
+
+* @property CI_Email $email
+
+* @property CI_DB_active_record $db
+
+* @property CI_DB_forge $dbforge
+
+*/
 class CI_Model {
 
 	/**
