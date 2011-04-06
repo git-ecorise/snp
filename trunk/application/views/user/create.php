1eeae39081739996a15f8c4484e25bb2e1491267
bigslott@gmail.com <bigslott@gmail.com@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-06 22:34:31 +0000

    jeg er træt og vil gå i seng, ses

--- trunk/application/views/user/create.php
+++ trunk/application/views/user/create.php
@@ -1,2 +1,9 @@
 <h1>Create user</h1>
-Create user view goes here ...
+
+<form action="<?= createuser_route() ?>" method="post">
+<label for="email">Email:</label><input type="text" name="email" value=""/><br/>
+<label for="password">Password:</label><input type="password" name="password" value=""/><br/>
+<label for="firstname">First name:</label><input type="text" name="firstname" value=""/><br/>
+<label for="lastname">Last name:</label><input type="text" name="lastname" value=""/><br/>
+<input type="submit" name="submit" value="Create user"/><br/>
+</form>
