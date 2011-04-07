1ba9a7d56a2e9dee4db9f591cbdd94ff44212a5d
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-07 12:40:32 +0000

    Renamed identification to authentication and added authentication class (service)

---
+++ trunk/application/libraries/authentication/Authentication.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once 'iIdentity.php';
+
+// Authentication - SERVICE ? + interface og smid under services folder ?
+
+class Authentication implements iAuthentication
+{
+    protected $key = 'user';
+    protected $CI;
+
+    function __construct($config = array())
+    {
+        // Get CodeIginiter reference
+        $this->CI =& get_instance();
+
+        // Config ?
+    }
+
+    public function login(iIdentity $identity)
+    {
+        // Add user to session
+        $this->CI->session->set_userdata($this->key, $identity);
+    }
+
+    public function logout()
+    {
+        // Remove user from session
+        $this->CI->session->unset_userdata('user');
+    }
+
+    public function get_user()
+    {
+        $user = $this->CI->session->user_data('user');  // returns false if not found
+
+        if (!$user)
+            return new DefaultUser();
+
+        return $user;
+    }
+
+}
+
+?>
