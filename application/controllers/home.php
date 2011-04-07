debcf39d90f82705fc471113be725f2df9775e80
super <super@lynhurtig.dk>
2011-04-07 20:57:52 +0000

    Fixed Home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,8 +4,6 @@ class Home extends CI_Controller
 {
     public function index()
     {
-        $this->session->set_flashdata('status', 'Hello, and welcome!');
-        
         $this->template->load("home/index");
     }
 }
