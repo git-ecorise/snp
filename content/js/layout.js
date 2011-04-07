03e95303436ef9f02752dd3f668273b2bd3f67da
bigslott <bigslott@gmail.com>
2011-04-07 20:57:34 +0000

    popup css og js lavet

--- content/js/layout.js
+++ content/js/layout.js
@@ -1,10 +1,30 @@
 (function ($)
-{
-    $(function ()
     {
-        // DOM READY
+        $(function ()
+        {
+            // DOM READY
+            //function for centering popup
+            function centerPopup(){
+                //request data for centering
+                var windowWidth = document.documentElement.clientWidth;
+                var windowHeight = document.documentElement.clientHeight;
+                var popupHeight = $(".message-box").height();
+                var popupWidth = $(".message-box").width();
+                //centering
+                $(".message-box").css({
+                    "position": "absolute",
+                    "top": (windowHeight/100*25)-popupHeight/2,
+                    "left": windowWidth/2-popupWidth/2
+                });
+            }
 
+            //call the centering function
+            centerPopup();
 
-        
-    });
-})(jQuery);
+            //check if there is a message and display it if so
+            if(!status == ""){
+                $('.message-box').append("<h1>" + status + "</h1>").fadeIn(500).delay(2000).fadeOut(500);
+            }
+        });
+
+    })(jQuery);
