a2bf322fbe55df97a922769be58f7dc010d5a5fe
super <super@lynhurtig.dk>
2011-04-07 21:36:37 +0000

    Fixed box

--- content/js/layout.js
+++ content/js/layout.js
@@ -3,24 +3,10 @@
         $(function ()
         {
             // DOM READY
-            //function for centering popup
-            function centerPopup(){
-                //request data for centering
-                var windowWidth = document.documentElement.clientWidth;
-                var windowHeight = document.documentElement.clientHeight;
-                var popupHeight = $(".message-box").height();
-                var popupWidth = $(".message-box").width();
-                //centering
-                $(".message-box").css({
-                    "left": windowWidth/2-popupWidth/2
-                });
-            }
 
-            //call the centering function
-            centerPopup();
-
-            //check if there is a message and display it if so
-            if(!status == ""){
+            // Display status message if set
+            if(status != "")
+            {
                 $('.message-box').append("<h1>" + status + "</h1>").fadeIn(500).delay(2000).fadeOut(500);
             }
         });
