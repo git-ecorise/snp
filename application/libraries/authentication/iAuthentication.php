86c9974dca50e619cc60cd11c87fd8f064fc1da9
super <super@lynhurtig.dk>
2011-04-07 12:40:32 +0000

    Renamed identification to authentication and added authentication class (service)

---
+++ application/libraries/authentication/iAuthentication.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once 'iIdentity.php';
+
+interface iAuthentication
+{
+    public function login(iIdentity $identity);
+    public function logout();
+}
+
+?>
