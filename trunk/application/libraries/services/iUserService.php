6381319bc14b167d6942fb37ff147c23bc744fed
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-07 17:58:29 +0000

    UserService and Authentication

--- trunk/application/libraries/services/iUserService.php
+++ trunk/application/libraries/services/iUserService.php
@@ -1,10 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once 'UserCreate.php';
+require_once 'UserLogin.php';
 
 interface iUserService
 {
-    public function create(UserCreate $create);             // User ?
+    public function create(UserCreate $create);
     public function authenticate(UserLogin $login);
-    public function validate(UserActivate $activation);
+    //public function validate(UserActivate $activation);
 }
 
 ?>
