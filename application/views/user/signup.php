995e0b97fc725e375ef50bbb87b1a36525115072
super <super@lynhurtig.dk>
2011-04-08 01:21:07 +0000

    Views

---
+++ application/views/user/signup.php
@@ -0,0 +1,36 @@
+<?php
+    $template_model->title = 'Create user';
+?>
+
+<h1>Sign up</h1>
+
+<form action="<?= signup_route() ?>" method="post">
+    <div class="input">
+        <label for="email">Email:</label>
+        <br/>
+        <input type="text" name="email" value="<?= set_value('email')?>"/>
+    </div>
+    <div class="input">
+        <label for="password">Password:</label>
+        <br/>
+        <input type="password" name="password" value="<?= set_value('password')?>"/>
+    </div>
+    <div class="input">
+        <label for="paswordConfirm">Confirm password:</label>
+        <br/>
+        <input type="password" name="password" value=""/>
+    </div>
+    <div class="input">
+        <label for="firstname">First name:</label>
+        <br/>
+        <input type="text" name="firstname" value=""/>
+    </div>
+    <div class="input">
+        <label for="lastname">Last name:</label>
+        <br/>
+        <input type="text" name="lastname" value=""/>
+    </div>
+    <div class="input-submit">
+        <input type="submit" name="submit" value="Sign up"/>
+    </div>
+</form>
