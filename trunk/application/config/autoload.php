4f231de77dc1e33327010dbb5ad3c5ffe97c43c2
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-09 18:49:47 +0000

    fixed settings for autoload form_validation, and template

--- trunk/application/config/autoload.php
+++ trunk/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array(APPPATH.'third_party');
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('template','database','session', 'form_validation', 'authentication/Authentication');
+$autoload['libraries'] = array('templateengine/Template','database','session', 'form_validation', 'authentication/Authentication');
 
 
 /*
