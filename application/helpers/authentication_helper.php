347c35f049f505d38050840c6a29bfb0359f537f
super <super@lynhurtig.dk>
2011-04-09 20:26:05 +0000

    Fixed user controller and authentication_helper.php

--- application/helpers/authentication_helper.php
+++ application/helpers/authentication_helper.php
@@ -1,18 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-require_once(APPPATH . 'libraries/authentication/AuthenticatedUser.php');
-require_once(APPPATH . 'libraries/authentication/DefaultUser.php');
-require_once 'route_helper.php';
-
-// Refactor this sometime
-// How do i fix the require_once ? not optimal that specific types have to be loaded
 
 function get_user()
 {
-    $CI =& get_instance();
-    //$CI->load->library('authentication/Authentication');
-
-    // Fix !
-    
+    $CI =& get_instance();   
     return $CI->authentication->get_user();
 }
 
