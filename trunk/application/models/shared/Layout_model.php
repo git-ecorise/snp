9e139cf63ab44e800c68ea96d3ac0b8eae14ef82
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-09 17:56:47 +0000

    fix

--- trunk/application/models/shared/Layout_model.php
+++ trunk/application/models/shared/Layout_model.php
@@ -6,10 +6,17 @@
 // get_head_elements ?
 // user object mm ? helper metoder til logged in ?
 
+// use methods set / get_title ? or allow direct access via public ? - fix in all views
+// also fix the identifier
+
+// Fix name to LayoutModel ?
+
+
+// Not much here yet - extend it with whatever you need to use in the layout template
+
 class Layout_model extends CI_Model
 {
-    //var $head = array();
-    var $title = '';
+    private $title = '';
 
     function __construct()
     {
