5b4c1259213ef6c4ebdd392c6b755bec579f9a3c
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-09 21:06:34 +0000

    Fixed UserModel

--- trunk/application/models/UserModel.php
+++ trunk/application/models/UserModel.php
@@ -11,28 +11,32 @@ class UserModel extends CI_Model implements iUserModel
     public function get_by_email($email)
     {
         $query = $this->db->get_where('user', array('email' => $email), 1);
+
         return $query->row();
     }
 
     public function insert($user)
     {
-        // return id later for validation
+        // implement returning the created user id for validation (insert_id)
         $this->db->insert('user', $user);
     }
 
     public function validate($code)
     {
-        // danger ... just quick impl - should be removed and use both id and code
+        // Should include the user id for proper security
+        // Also it allows to validate more than one time because where isactivated == FALSE is not included
         $this->db->where('activationcode', $code);
         $this->db->update('user', array('isactivated' => TRUE));
+
         return $this->db->affected_rows() > 0;
     }
   
     public function get_all_by_name($name)
     {
+        // Very quick search implementation - surely have to be improved someday ...
+        // Etc split $name by ' ' and use every word instead
         $this->db->like('LOWER(firstname)', strtolower($name));
         $this->db->or_like('LOWER(lastname)', strtolower($name));
-        
         $this->db->where('isactivated', TRUE);
         $query = $this->db->get('user');
 
