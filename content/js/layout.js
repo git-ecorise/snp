1d3563a632d54997c2f2ef0b23e9db4d9c97c559
super <super@lynhurtig.dk>
2011-04-09 19:51:18 +0000

    Fixed Layout, header and Stylesheet

--- content/js/layout.js
+++ content/js/layout.js
@@ -1,14 +1,13 @@
 (function ($)
+{
+    $(function ()
     {
-        $(function ()
+        // DOM READY
+
+        // Display status message if set
+        if(status != "")
         {
-            // DOM READY
-
-            // Display status message if set
-            if(status != "")
-            {
-                $('.message-box').append("<h1>" + status + "</h1>").fadeIn(500).delay(2000).fadeOut(500);
-            }
-        });
-
-    })(jQuery);
+            $('#message-box').append("<h1>" + status + "</h1>").fadeIn(500).delay(2000).fadeOut(500);
+        }
+    });
+})(jQuery);
