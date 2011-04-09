61cb043cc1ac9db64c9ddc6e073521059da4628e
super <super@lynhurtig.dk>
2011-04-09 20:57:05 +0000

    Login fixed and helper created

--- application/helpers/authentication_helper.php
+++ application/helpers/authentication_helper.php
@@ -1,5 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+// Remmber later on there can be a difference between been authenticatd and authorized
+
 function get_user()
 {
     $CI =& get_instance();   
@@ -21,6 +23,21 @@ function ensure_authorized()
         $CI->session->set_flashdata('status', 'You are not allowed to view the page. Please login.');
         // returns immediatly
         redirect(login_route());
+    }
+}
+
+function redirect_if_authenticated($status = '')
+{
+    if (get_user()->is_authenticated())
+    {
+        if ($status != '')
+        {
+            $CI =& get_instance();
+            $CI->session->set_flashdata('status', $status);
+        }
+        
+        // returns immediatly
+        redirect (home_route());
     }
 }
 
