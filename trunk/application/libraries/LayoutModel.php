16f88d589dcbf6d713be0cf4f1d3201ce02b9ec1
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-24 18:05:27 +0000

    More

--- trunk/application/libraries/LayoutModel.php
+++ trunk/application/libraries/LayoutModel.php
@@ -2,6 +2,9 @@
 
 // Not much here yet - extend it with whatever you need to use in the layout template
 
+// Move ? to models ? view model ? /view/shared ?
+// extend CI_Model ? or not really needed ? - use load->model to load in template engine ? 
+
 class LayoutModel
 {
     private $title = '';
