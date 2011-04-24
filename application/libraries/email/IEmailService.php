f11d3cc8e8599fb7e7d8ccf700f62993dc0cc0f3
super <super@lynhurtig.dk>
2011-04-24 17:41:33 +0000

    Deleted old service folder and moved EmailService to email folder

---
+++ application/libraries/email/IEmailService.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once (APPPATH . 'models/input/IUserSignUp.php');
+
+interface IEmailService
+{
+    public function send_signup_email(IUserSignUp $user);
+}
+
+?>
--- application/libraries/services/iEmailService.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-interface iEmailService
-{
-    public function send_validation_email($email, $code);
-}
-
-?>
