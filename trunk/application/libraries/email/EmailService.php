65d79adfc08921c23bfc9495993ef03dcf3ed22f
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-24 17:41:33 +0000

    Deleted old service folder and moved EmailService to email folder

--- trunk/application/libraries/email/EmailService.php
+++ trunk/application/libraries/email/EmailService.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-require_once 'iEmailService.php';
+require_once 'IEmailService.php';
 
-class EmailService implements iEmailService
+class EmailService implements IEmailService
 {
     protected $CI;
     protected $from;
@@ -12,18 +12,29 @@ class EmailService implements iEmailService
         // Get CodeIginiter reference
         $this->CI =& get_instance();
 
+        // Load the email library
+        $this->CI->load->library('email');
+
         // Load Config
         $this->from = $config['from'];
         $this->name = $config['name'];
+
+        // Setup defaults
+        $this->CI->email->from($this->from, $this->name);     
     }
 
-    public function send_validation_email($email, $code)
+    public function send_signup_email(IUserSignUp $user)
     {
-        $this->CI->load->library('email');
+        // Create email
+        $this->CI->email->to($user->get_email());
 
-        // Setup
-        $this->CI->email->from($this->from, $this->name);
-        $this->CI->email->to($email);
+
+        // Include the id somehow, because it is needed when validating the email ?
+        // Split validationcode out into seperate table and delete when no longer needed ?
+
+        // but if id isnt found it will require you to enter both the validation code and your email to validate it - so it can look up the id ?
+
+        // Why not always use the email then ? its less predictable to ...
 
         $this->CI->email->subject('Validate your email - ' . $this->name);
         $this->CI->email->message('Please validate your email.<br /><a href="' + validate_route($code) + '">Click here to validate</a>');
