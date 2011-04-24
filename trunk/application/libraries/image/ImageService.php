41c31999b5be7278a77a184b852ab05a908409a9
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-24 17:46:50 +0000

    Image service

---
+++ trunk/application/libraries/image/ImageService.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once 'IImageService.php';
+
+class ImageService implements IImageService
+{
+    function __construct()
+    {
+    }
+}
+
+?>
