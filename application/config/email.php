4ca3ef8b98a00da6b351766b92a3d4619349c88f
super <super@lynhurtig.dk>
2011-04-25 18:42:18 +0000

    Email is now working

--- application/config/email.php
+++ application/config/email.php
@@ -3,11 +3,14 @@
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
 $config['smtp_port'] = '465';
+$config['smtp_timeout'] = '30';
+
 $config['smtp_user'] = 'carlsagangroup@gmail.com';
 $config['smtp_pass'] = 'CarlS1234';
 
 $config['mailtype'] = 'html';
 
+$config['charset'] = 'utf-8';
 $config['crlf'] = '\r\n';
 $config['newline'] = '\r\n';
 
