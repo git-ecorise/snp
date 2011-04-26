df1b241f87e0c63c0ffe0e1acdf368e3f16c8f39
super <super@lynhurtig.dk>
2011-04-26 00:39:08 +0000

    Deleted email service settings

--- application/config/email.php
+++ application/config/email.php
@@ -1,20 +1,27 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'ssl://smtp.googlemail.com';
-$config['smtp_port'] = '465';
-$config['smtp_timeout'] = '5';
+$config = Array(
+    'protocol'      => "smtp",
+    'smtp_host'     => "ssl://smtp.googlemail.com",
+    'smtp_port'     => 465,
+    'smtp_timeout'  => "5",
+    'smtp_user'     => "socialnetworkp@gmail.com",
+    'smtp_pass'     => "Tester1234",
+
+    'mailtype'      => "html",
+    'charset'       => "iso-8859-1",
+    //'charset'     => 'utf-8',
+    'newline'       => "\r\n",
+    'crlf'          => "\r\n"
+
+    // to
+    // name
+    
+);
+
+
 
 //$config['smtp_user'] = 'carlsagangroup@gmail.com';
 //$config['smtp_pass'] = 'CarlS1234';
-
-$config['smtp_user'] = 'socialnetworkp@gmail.com';
-$config['smtp_pass'] = 'Tester1234';
-
-//$config['mailtype'] = 'html';
-
-$config['charset'] = 'utf-8';
-$config['crlf'] = '\r\n';
-$config['newline'] = '\r\n';
 
 ?>
--- application/config/emailservice.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['from'] = 'socialnetworkp@gmail.com';
-$config['name'] = 'The Social Network';
-
-?>
