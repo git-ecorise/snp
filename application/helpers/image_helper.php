545522bce0832bab7db65bbb4407ba64e8aac833
super <super@lynhurtig.dk>
2011-04-27 22:25:16 +0000

    Helper

---
+++ application/helpers/image_helper.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function thumbnail_default_image()
+{
+    return image_content('thumbnail-default.jpg');      // Korrekte navn ?
+}
+
+function profile_default_image()
+{
+    return image_content('profile-default.jpg');      // Korrekte navn ?
+}
+
+function thumbnail_image($id)
+{
+    return image_content($id . '/thumbnail.jpg');       // img/id/thumbnail.jpg
+}
+
+function profile_image($id)
+{
+    return image_content($id . '/profile.jpg');
+}
+
+function select_thumbnail_image($id, $haspicture)        // navn ?
+{
+    if ($haspicture)
+        return thumbnail_image($id);
+    else
+        return thumbnail_default_image();
+}
+
+function select_profile_image($id, $haspicture)        // navn ?
+{
+    if ($haspicture)
+        return profile_image($id);
+    else
+        return thumbnail_default_image();
+}
+
+?>
