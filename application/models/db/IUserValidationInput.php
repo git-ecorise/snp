f7cdd399a035d0fa13e84da696bd33154e13b4db
super <super@lynhurtig.dk>
2011-04-27 11:56:39 +0000

    Models

--- application/models/db/IUserValidationInput.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-interface IUserValidationInput
-{
-    public function get_email();
-    public function get_validationcode();
-}
-
-?>
--- application/models/input/IValidatable.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-interface IValidatable
-{
-    public function is_valid();     // validate ?
-}
-
-?>
