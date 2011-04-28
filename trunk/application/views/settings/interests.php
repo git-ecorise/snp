8c242d17900478d3d6ba82ea9ad94b57f39682c5
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-28 14:30:18 +0000

    fix

--- trunk/application/views/settings/interests.php
+++ trunk/application/views/settings/interests.php
@@ -6,7 +6,7 @@
     <div class="input">
         <div class="ui-widget">
             <label for="interests">Interest: </label>
-            <textarea cols="50" rows="5" name="interests" id="interests"><?=$interests;?></textarea>
+            <textarea cols="50" rows="5" name="interests" id="interests"><?= $interests; ?></textarea>
         </div>
         <div class="input-submit">
             <input type="submit" name="submit" id="submit" value="Save" />
