4c45ea30546539c49a378b1026710787bbc1c052
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-28 14:13:01 +0000

    Fixes

--- trunk/application/views/home/index.php
+++ trunk/application/views/home/index.php
@@ -1,1 +1,9 @@
-<h1>home</h1>
+<?php
+    $template_model->set_title('Home');
+?>
+
+<h1>Home</h1>
+
+<div>Teaser text. Join and find your friends to keep in contact.</div>
+
+<div>Latest Users</div>
