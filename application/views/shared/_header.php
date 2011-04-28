4fe568cf989cd92ac20f1d2ff862aab573a139f8
super <super@lynhurtig.dk>
2011-04-28 16:04:30 +0000

    Fixed home stuff

--- application/views/shared/_header.php
+++ application/views/shared/_header.php
@@ -5,13 +5,15 @@
 <a href="<?= home_route() ?>">
     <img src="<?= graphic_content('logo.png') ?>" alt="logo"/>
 </a>
+
 <div id="navigation">
     <ul>
-        <li><a href="<?= profile_route() ?>">Profile</a></li>
-        <?if (!$user->is_authenticated()):?>
+        <?if (!is_authenticated()):?>
+            <li><a href="<?= home_route() ?>">Home</a></li>
             <li><a href="<?= signup_route() ?>">Sign up</a></li>
             <li><a href="<?= login_route() ?>">Login</a></li>
-        <?else:?>
+        <? else: ?>
+            <li><a href="<?= profile_route() ?>">Home</a></li>
             <li><a href="<?= usersearch_route() ?>">Search</a></li>
             <li><a href="<?= settings_route() ?>">Settings</a></li>
             <li><a href="<?= logout_route() ?>">Logout</a></li>
