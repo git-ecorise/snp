907e8943f165a73fc9d00006a79345121ab4d86e
super@lynhurtig.dk <super@lynhurtig.dk@603ad204-ddc4-73f5-8310-8291a396b776>
2011-04-29 02:40:18 +0000

    fix

--- trunk/application/models/InputBase.php
+++ trunk/application/models/InputBase.php
@@ -11,17 +11,6 @@ abstract class InputBase extends CI_Model implements IValidatable
         // Set delimiters
         $this->form_validation->set_error_delimiters('<span class="error">', '</span>');
 
-
-        
-
-        // Only run validation if validation group is set ?
-        // Make possible to disable ? but why would you use it then ?
-
-        // Hent kun data hvis validation returnerer true ? loaddata event ? der kaldes hvis is_valid returnerer true !? 
-
-
-
-
         // Run Validation immediately before input is loaded
         // Because input data can also be modified in the validation process.
         $this->form_validation->run($validationgroup);
